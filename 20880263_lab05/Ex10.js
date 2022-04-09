88228863819de915e013776f83501003a260434d
Nam Nguyen <nhnkg123@gmail.com>
2022-04-09 23:18:32 +0700

    homework (lab05) finished

---
+++ 20880263_lab05/Ex10.js
@@ -0,0 +1,18 @@
+function addItem(){
+    var item = document.getElementById("item");
+    if (item.value != ""){
+        var ul = document.getElementById("todoList");
+        var li = document.createElement("li")
+
+        li.innerHTML = item.value + " (<a onclick='delItem(this.parentNode);' href='#'>remove</a>)";
+
+        ul.appendChild(li);
+        return true;
+    }
+    return false;
+}
+
+function delItem(li){
+    var ul = li.parentNode;
+    ul.removeChild(li);
+}
